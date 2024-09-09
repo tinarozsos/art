a3c2a73534d5bcbb354e458ba10dfee2f44195c4
Tina Rozsos <76408420+tinarozsos@users.noreply.github.com>
2024-09-09 15:16:42 +0200

    add galleries

---
+++ make_gallery.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+pages <- list.dirs("fig") |> str_extract("(?<=/).*") |> na.omit()
+
+for (page in pages) {
+  files <- list.files(paste0("fig/", page), full.names = TRUE)
+  img <- map_chr(files, ~ paste0('<img style="float: left;" src="', ., '" width="450"/>')) |> 
+    paste(collapse = "\n")
+  
+  paste0(
+    '<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    img {
+      margin: 10px;
+    }
+    </style>
+    </head>
+    <body>',
+    img,
+    '</body>
+    </html>'
+  ) |> writeLines(paste0(page, ".html"))
+}
